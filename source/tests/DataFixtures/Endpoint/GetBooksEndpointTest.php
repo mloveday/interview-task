1c7cd62709e2d9aa862a5ceb1fcc6252ac34b5b5
miles <miles.loveday@gmail.com>
2019-05-18 22:32:46 +0100

    GetBooksEndpointTest tests the contents of the response

--- source/tests/DataFixtures/Endpoint/GetBooksEndpointTest.php
+++ source/tests/DataFixtures/Endpoint/GetBooksEndpointTest.php
@@ -8,6 +8,7 @@ use App\Entity\Book;
 class GetBooksEndpointTest extends DataFixtureTest {
     public function test_getsListOfAllBooks() {
         // given
+        /** @var Book[] $allBooks */
         $allBooks = $this->entityManager->getRepository(Book::class)->findAll();
 
         // when
@@ -16,5 +17,12 @@ class GetBooksEndpointTest extends DataFixtureTest {
 
         // then
         $this->assertSameSize($allBooks, $responseObject);
+        foreach($allBooks as $book) {
+            $foundBooks = array_filter($responseObject, function($object) use ($book) { return $object->id === $book->getId();});
+            $this->assertCount(1, $foundBooks, "Expected exactly 1 book to match id {$book->getId()}, found " . count($foundBooks));
+            $foundBook = current($foundBooks);
+            $this->assertEquals($book->getTitle(), $foundBook->title, "Title mismatch for book with id {$book->getId()}");
+            $this->assertEquals($book->getAuthor(), $foundBook->author, "Author mismatch for book with id {$book->getId()}");
+        }
     }
 }
